937d2c1f7dddd0235f00e515392a9ed1b5dd1d4d
Soyeb7 <Soyeb277@gmail.com>
2020-07-17 12:21:27 +0100

    Updates

---
+++ precache-manifest.ca4ff0829ce962c4353a637a7277cd18.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4fe589c3debdeee5516cc63b40fcfad4",
+    "url": "/cloudinversion/index.html"
+  },
+  {
+    "revision": "03e887b77594e4d19f45",
+    "url": "/cloudinversion/static/css/2.4819d237.chunk.css"
+  },
+  {
+    "revision": "598c38d7984dedf118be",
+    "url": "/cloudinversion/static/css/main.91872ab2.chunk.css"
+  },
+  {
+    "revision": "03e887b77594e4d19f45",
+    "url": "/cloudinversion/static/js/2.55faabd2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cloudinversion/static/js/2.55faabd2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "598c38d7984dedf118be",
+    "url": "/cloudinversion/static/js/main.9764d5e5.chunk.js"
+  },
+  {
+    "revision": "84855db905429744fa01",
+    "url": "/cloudinversion/static/js/runtime-main.f177dff1.js"
+  }
+]);
