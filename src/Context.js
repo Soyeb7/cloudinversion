6c60e4c992ea6c21f8781533b89b172fd81a29f6
Soyeb7 <Soyeb277@gmail.com>
2024-10-20 15:54:21 +0100

    remove unused import

--- src/Context.js
+++ src/Context.js
@@ -1,5 +1,4 @@
 import { createContext } from 'react'
-import Content from './components/Content'
 
 // Context lets you create one instance of the data that you want other components to use. This data can then be accessed by any component
 
